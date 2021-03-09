7b371e6865abb00a7478b932e512ef21a5fc96ad
Kesiu <kesiu.pro@gmail.com>
2021-03-09 03:21:49 +0100

    Vue Instance  - Data and Methods

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 new Vue({
   el: '#vue-app', // Selector to Vue Instance.
   data: { // Data of Vue Instance.
-    name: 'Shaun'
+    name: 'Shaun',
+    job: 'Manager'
+  },
+  methods: {
+    greet: function(){
+      return 'Good Morning' + this.name; // this.data.name == this.name
+    },
+    hello: function(name){
+      return 'Hello ' + name;
+    },
   }
 });
