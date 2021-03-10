0acb277ddca4932ea2965241a9587f3e9038d1cc
Kesiu <kesiu.pro@gmail.com>
2021-03-10 22:47:57 +0100

    Data Binding

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ new Vue({
   el: '#vue-app', // Selector to Vue Instance.
   data: { // Data of Vue Instance.
     name: 'Shaun',
-    job: 'Manager'
+    job: 'Manager',
+    website: 'https://www.google.com/',
+    websiteTag: '<a href="https://www.google.com/">Google</a>',
   },
   methods: {
     greet: function(){
