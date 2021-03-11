9df059396cad959479b008948cbf158ec37d7b4d
Kesiu <kesiu.pro@gmail.com>
2021-03-11 20:30:57 +0100

    Events

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ new Vue({
   data: { // Data of Vue Instance.
     name: 'Shaun',
     job: 'Manager',
+    age: 50,
     website: 'https://www.google.com/',
     websiteTag: '<a href="https://www.google.com/">Google</a>',
+    x: 0,
+    y: 0,
   },
   methods: {
     greet: function(){
@@ -12,6 +15,22 @@ new Vue({
     },
     hello: function(name){
       return 'Hello ' + name;
+    },
+    add: function(){
+      this.age++;
+    },
+    subtract: function(){
+      this.age--;
+    },
+    addAge: function(number){
+      this.age = this.age + number;
+    },
+    subtractAge: function(number){
+      this.age = this.age - number;
+    },
+    updateXY: function(event){
+      this.x = event.offsetX;
+      this.y = event.offsetY;
     },
   }
 });
